d6b3a43b29197b6417cdabcbbbabe7db7b7d8852
GaYoNE <91003766+GaYoNE00@users.noreply.github.com>
2023-10-04 10:38:45 +0900

    search_entity 수정

--- src/main/java/com/all4land/asd/entity/search_entity.java
+++ src/main/java/com/all4land/asd/entity/search_entity.java
@@ -1,4 +1,23 @@
 package com.all4land.asd.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class search_entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long search_id;
+
+    private String search_name;
 }
